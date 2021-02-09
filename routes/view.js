ec650b289894a804d395309ce02b565731f593a3
Matthew Guillen <matthewguillen777@gmail.com>
2021-02-08 22:12:52 -0500

    routes to home and notes work

--- routes/view.js
+++ routes/view.js
@@ -1,8 +1,11 @@
 const path = require("path");
 const router = require("express").Router();
-router.get("/", function(req, res) {
+
+// Directs to home page on initial load
+router.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
 });
+// Route to notes page
 router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
