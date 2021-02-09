45e3b8a8b7e67da5c9899a00bb17e61744a8917f
Matthew Guillen <matthewguillen777@gmail.com>
2021-02-08 22:19:15 -0500

    directs to home pg if url invalid

--- routes/view.js
+++ routes/view.js
@@ -1,13 +1,13 @@
 const path = require("path");
 const router = require("express").Router();
 
-// Directs to home page on initial load
-router.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
-});
 // Route to notes page
 router.get("/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
+});
+// Directs to home page
+router.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 module.exports = router;
