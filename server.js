56fa4b0b46c243e217d77ddbb3b12a37fdac3932
Matthew Guillen <matthewguillen777@gmail.com>
2021-02-08 21:42:13 -0500

    finalized server

--- server.js
+++ server.js
@@ -15,7 +15,6 @@ const PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
-// app.use("/api", apiRoutes);
 app.use("/", viewRoutes);
 app.use("/api", apiRoutes);
 
