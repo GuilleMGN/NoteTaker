1df02d7c75e2e2d208e8e5166cc6dc87c5d64238
Matthew Guillen <matthewguillen777@gmail.com>
2021-02-08 22:01:43 -0500

    finished api functions

--- routes/api.js
+++ routes/api.js
@@ -1,16 +1,20 @@
 const path = require("path");
 const router = require("express").Router();
 const notes = require("../db/Notes.js");
+
+// Retrieves notes
 router.get("/notes", function(req, res) {
     notes.getNotes().then(notes => {
         res.json(notes);
     }).catch(err => res.status(500).json(err))
 });
+// Saves notes
 router.post("/notes", function(req, res) {
     notes.writeNote(req.body).then(notes => {
         res.json(notes);
     }).catch(err => res.status(500).json(err))
 });
+// Deletes notes
 router.delete("/notes/:id", function(req, res) {
     notes.deleteNote(req.params.id).then(() => res.json({
         ok: true
